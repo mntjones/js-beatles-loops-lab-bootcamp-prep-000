35968373af7ab8ada3cab2c66829cb32730d00c1
Monica Tamaru Jones <mntamaru@gmail.com>
2017-12-21 19:05:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 function theBeatlesPlay(musician, instrument) {
+  var arr = []
   
+  if (musician.length !== instrument.length) {
+    console.log("Input arrays are of unequal length")
+    return
+  }
+  
+  for (var i = 0; i < musician.length; i++) {
+    arr[i] = musician[i] + " plays " + instrument[i]
+  }
+  
+  return arr
 }
